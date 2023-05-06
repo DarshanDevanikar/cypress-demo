917b32e1ab7414425f15c8e73d3f6dad1b96e80f
Darshan Devanikar <darshandevanikar@gmail.com>
2023-05-06 22:31:28 +0530

    added base URL

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'ptfd7c',
   e2e: {
+    baseUrl: 'https://example.cypress.io',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
